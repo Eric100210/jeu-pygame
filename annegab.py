f5780191733ee59eebb06d4545b091f9544409c4
Anne-Gabrielle Gibeili <anne-gabrielle.gibeili@etu.minesparis.psl.eu>
2023-11-18 15:25:12 +0100

    Création de la fenêtre de jeu

--- annegab.py
+++ annegab.py
@@ -1,1 +1,22 @@
-dossier annegab
+import pygame as pg
+pg.init()
+
+#générer la fenêtre de notre jeu
+pg.display.set_caption("Kill the dementors")
+screen = pg.display.set_mode((1200,900))
+background = pg.image.load('background.png')
+
+running = True
+
+#Boucle tant que cette condition est vraie
+while running:
+
+    #appliquer l'arrière plan de notre jeu
+    screen.blit(background, (0,0))
+    pg.display.flip() #on met à jour
+
+    #si le joueur veut quitter le jeu
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+            pg.quit()
